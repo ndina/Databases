de4c0171acfd5a4bec62f531b7890d081778bd3b
ndina <31282816+ndina@users.noreply.github.com>
2018-09-07 14:09:41 +0600

    lab1

---
+++ lab1.sql
@@ -0,0 +1,30 @@
+CREATE DATABASE   db1;
+
+CREATE TABLE userss(
+  id SERIAL PRIMARY KEY ,
+  firstname VARCHAR(50) NOT NULL,
+  lastname VARCHAR(50) NOT NULL
+);
+
+ALTER TABLE userss
+  ADD COLUMN isadmin INT;
+
+SELECT * FROM userss;
+
+ALTER TABLE userss ALTER COLUMN isadmin TYPE BOOLEAN using isadmin:: BOOLEAN;
+
+ALTER TABLE userss
+    ALTER COLUMN isadmin SET DEFAULT FALSE;
+
+ALTER TABLE userss ADD CONSTRAINT userss_pkey PRIMARY KEY(id);
+
+CREATE TABLE tasks(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  user_id INT
+);
+
+DROP TABLE tasks;
+DROP DATABASE db1;
+
+
